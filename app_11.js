be207c4bf030c43e4f667bb61d4229ba1939c71c
GUENFOUDI MOHAMED <mohamed.guenfoudi@hotmail.com>
2022-04-17 22:00:22 +0100

    Update app_11.js

--- app_11.js
+++ app_11.js
@@ -1,8 +1,7 @@
-// app_11
+// // app_11
 
 const li = document.querySelectorAll('body .container .navBar .list li');
 const article = document.querySelectorAll('body .container .contents .main .article');
-
 for(let i = 0;i<li.length;i++){
     li[i].addEventListener('click',function(){
         for(let x = 0;x<article.length;x++){
@@ -14,4 +13,4 @@ for(let i = 0;i<li.length;i++){
             }   
         }
     });
-}
+};
